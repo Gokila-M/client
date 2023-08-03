64e45ba883ec4440401573603a81fdd17119bdf8
chhavi <chhavi@codiis.com>
2023-08-03 11:59:46 +0530

    home,hotel,navbar,list,header,header-css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import "./home.css"
+import Navbar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+     <Navbar/>
+     <Header/>
     </div>
   )
 }
